893d002ad94f04e94f5a4af7db55d4ac57e0286c
Jules James <jules.james594@gmail.com>
2025-04-29 15:27:40 +0100

    functions

--- app/pokemon_stats.py
+++ app/pokemon_stats.py
@@ -5,39 +5,34 @@ from transform_image import  get_image
 
 # User input field 
 
-#st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
-
 df = extract_and_clean_data('../data/pokemon.csv')
-#print(df)
 
 
-poke = st.number_input("Enter Pokemon ID")
+def enter_pokemon_id(df):
+    poke = st.number_input("Enter Pokemon ID")
+    df.loc[poke,'name']
+    st.write("Your selected Pokemon is", poke)
+    st.header(f"You caught {poke}!")
+    return df.loc[poke,'name']
 
-pokemon = df[df['pokedex_number'] == poke]
-#name = "".join(list(pokemon['name']))
-name = df.loc[poke,'name']
-
-st.write("Your selected Pokemon is", name)
-
-print(pokemon['name'])
-# Pokemon stats functions
 
 # Fetch Pokemon image url
-poke_image = get_image(df.loc[poke,"image_url"])
+def display_image(df,pokemon_name):
+    poke_image = get_image(df.loc[pokemon_name,"image_url"])
+    st.image(poke_image,width=200)
 
 # Pokemon stats display
-st.header(f"You caught {name}!")
-print("".join(list(pokemon['species'])))
-#st.image(poke_image)
 
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.metric(label ="Species", value=df.loc[poke,'species'])
-with col2:
-   st.metric("Height (m)", value=df.loc[poke,'height_m'])
+def display_stat_columns(df,pokemon_name):
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(label ="Species", value=df.loc[pokemon_name,'species'])
+    with col2:
+        st.metric("Height (m)", value=df.loc[pokemon_name,'height_m'])
+        
+    with col3:
+        st.metric("Weight (kg)", df.loc[pokemon_name,'weight_kg'])
     
-with col3:
-    st.metric("Weight (kg)", df.loc[poke,'weight_kg'])
-st.divider()
-st.dataframe(df.loc[poke])
+def display_df(df,pokemon_name):    
+    st.divider()
+    st.dataframe(df.loc[pokemon_name])
