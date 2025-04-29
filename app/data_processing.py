c9dc969d388f5033084b9d232e205f6c3e5f1763
Jules James <jules.james594@gmail.com>
2025-04-29 13:41:04 +0100

    data cleaning

---
+++ app/data_processing.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+
+def load_data(filepath):
+    """Load the Pokemon dataset from a CSV file."""
+    return pd.read_csv(filepath)
+
+
+def drop_columns(df):
+    """ Drop unused columns"""
+    columns_to_drop = [
+        df.columns[0], 'german_name', 'egg_type_number',
+        'japanese_name', 'egg_type_1', 'egg_type_2',
+        'egg_cycles', 'base_friendship', 'base_experience',
+        'growth_rate', 'against_normal', 'against_fire',
+        'against_water', 'against_electric', 'against_grass',
+        'against_ice', 'against_fight', 'against_poison',
+        'against_ground', 'against_flying', 'against_psychic',
+        'against_bug', 'against_rock', 'against_ghost',
+        'against_dragon', 'against_dark', 'against_steel', 'against_fairy',
+        'type_number', 'abilities_number', 'percentage_male', 'catch_rate'
+    ]
+    df.drop(columns_to_drop, axis=1, inplace=True)
+    return df
+
+
+def drop_empty_rows(df):
+    """ Drop rows with no abilities"""
+    df = df.dropna(subset='ability_1')
+    return df
+
+
+def clean_categorical_columns(df):
+    """ Fill null values with None"""
+    df['type_2'] = df['type_2'].fillna('None')
+    df['ability_2'] = df['ability_2'].fillna('None')
+    df['ability_hidden'] = df['ability_hidden'].fillna('None')
+    return df
+
+
+def load_and_clean_data(filepath):
+    """ Load and clean Pokemon dataset"""
+    df = load_data(filepath)
+    df = drop_columns(df)
+    df = drop_empty_rows(df)
+    df = clean_categorical_columns(df)
+    return df
