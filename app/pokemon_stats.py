d74270b0f0574993726db5f521d6817d2c440f01
Maria Grozova <marija.grozova@gmail.com>
2025-04-29 13:29:34 +0100

    add streamlit functions to display stats with placeholders

--- app/pokemon_stats.py
+++ app/pokemon_stats.py
@@ -9,5 +9,15 @@ st.write("Your selected Pokemon is", poke)
 
 # Pokemon stats functions
 
-# Pokemon stats display
+# Fetch Pokemon image url
+poke_image = 
 
+# Pokemon stats display
+st.header(f"You caught {poke_name}!")
+st.image(poke_image)
+col1, col2, col3 = st.columns(3)
+col1.metric("Species", )
+col2.metric("Height", )
+col3.metric("Weight", )
+st.divider()
+st.dataframe()
