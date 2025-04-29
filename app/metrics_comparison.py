6c65aec11d965059ed5f71540786d371d9cd956c
Maria Grozova <marija.grozova@gmail.com>
2025-04-29 11:21:49 +0100

    File setup for streamlit functions and visuals

---
+++ app/metrics_comparison.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import plotly.express as px
+
+# Metrics Functions
+
+
+# Pick additional random Pokemon
+
+
+# Calculate metrics
+
+
+# Visualisation functions
+
+
+# Comparison chart
+
