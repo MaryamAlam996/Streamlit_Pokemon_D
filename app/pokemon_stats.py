dba029fa37e45ce0dd0ddf2b4d296255bd83db02
Maria Grozova <marija.grozova@gmail.com>
2025-04-29 11:55:43 +0100

    add skeleton functions for user input and calculating stats for comparison

--- app/pokemon_stats.py
+++ app/pokemon_stats.py
@@ -3,6 +3,10 @@ import plotly.express as px
 
 # User input field 
 
+st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+poke = st.text_input("Pokemon name", "")
+st.write("Your selected Pokemon is", poke)
+
 # Pokemon stats functions
 
 # Pokemon stats display
