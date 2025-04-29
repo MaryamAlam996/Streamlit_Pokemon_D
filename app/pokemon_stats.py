2c6643d62eaeba8c4c69f1601b2a60f2746baf9e
Jules James <jules.james594@gmail.com>
2025-04-29 15:14:09 +0100

    streamlit

--- app/pokemon_stats.py
+++ app/pokemon_stats.py
@@ -1,23 +1,43 @@
 import streamlit as st
-import plotly.express as px
+from data_processing import extract_and_clean_data
+from transform_image import  get_image
+
 
 # User input field 
 
-st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
-poke = st.text_input("Pokemon name", "")
-st.write("Your selected Pokemon is", poke)
+#st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
 
+df = extract_and_clean_data('../data/pokemon.csv')
+#print(df)
+
+
+poke = st.number_input("Enter Pokemon ID")
+
+pokemon = df[df['pokedex_number'] == poke]
+#name = "".join(list(pokemon['name']))
+name = df.loc[poke,'name']
+
+st.write("Your selected Pokemon is", name)
+
+print(pokemon['name'])
 # Pokemon stats functions
 
 # Fetch Pokemon image url
-poke_image = 
+poke_image = get_image(df.loc[poke,"image_url"])
 
 # Pokemon stats display
-st.header(f"You caught {poke_name}!")
-st.image(poke_image)
+st.header(f"You caught {name}!")
+print("".join(list(pokemon['species'])))
+#st.image(poke_image)
+
+
 col1, col2, col3 = st.columns(3)
-col1.metric("Species", )
-col2.metric("Height", )
-col3.metric("Weight", )
+with col1:
+    st.metric(label ="Species", value=df.loc[poke,'species'])
+with col2:
+   st.metric("Height (m)", value=df.loc[poke,'height_m'])
+    
+with col3:
+    st.metric("Weight (kg)", df.loc[poke,'weight_kg'])
 st.divider()
-st.dataframe()
+st.dataframe(df.loc[poke])
